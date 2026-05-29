999f35487a2d8bba6ae497734af3eee7f55af194
Nikitarathod2001 <nikitarathod3601@gmail.com>
2026-05-29 19:42:08 +0530

    Initial project setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
